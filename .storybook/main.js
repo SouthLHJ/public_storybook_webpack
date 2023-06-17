180a36977c1efd51324eca548c2359ddafbf7785
SouthLHJ <107970775+SouthLHJ@users.noreply.github.com>
2023-06-17 21:07:02 +0900

    docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,21 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
+
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/react-webpack5",
     options: {},
   },
   docs: {
     autodocs: "tag",
   },
+  core: {
+    "builder": "@storybook/builder-webpack5"
+  }
 };
 export default config;
